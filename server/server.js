066d940e9747f2b6a8fcea6d62e50c6258999046
Toluwanimi Adeyemo <68569546+tolu-c@users.noreply.github.com>
2022-03-13 20:32:35 +0100

    this code works

--- server/server.js
+++ server/server.js
@@ -5,39 +5,50 @@ require("dotenv").config();
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const nodemailer = require("nodemailer");
+const nodeMailgun = require("nodemailer-mailgun-transport");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
-// const mailgunAuth = {
-//   auth: {
-//     api_key: "key-12341234123412341234",
-//     domain:
-//       "One of your domain names listed at your https://mailgun.com/app/domains",
-//   },
+// step 1
+const mailgunAuth = {
+  auth: {
+    api_key: process.env.API_KEY,
+    domain: "officialrectification.com",
+  },
+};
+
+// step 2
+// let transporter = nodemailer.createTransport(nodeMailgun(mailgunAuth));
+
+// step 3
+// const mailOptions = {
+//   from: process.env.MAIL_FROM,
+//   to: "toluadeyemo7@gmail.com",
+//   subject: "TEST EMAIL",
+//   html: `<h4>Here is your mail</h4>
+//         <p>${text}</p>`,
 // };
 
-app.post('/send-mail', cors(), async (req, res) => {
-    let {text} = req.body
-    const transport = nodemailer.createTransport({
-        host: process.env.MAIL_HOST,
-        port: process.env.MAIL_PORT,
-        auth: {
-            user: process.env.MAIL_USER,
-            password: process.env.MAIL_PASSWORD
-        }
-    })
+app.post("/send-mail", cors(), async (req, res) => {
+  let { text } = req.body;
+  const transport = nodemailer.createTransport(nodeMailgun(mailgunAuth));
 
-    await transport.sendMail({
-        from: process.env.MAIL_FROM,
-        to: 'toluadeyemo7@gmail.com',
-        subject: 'TEST EMAIL',
-        html: `<h4>Here is your mail</h4>
-        <p>${text}</p>`
-    })
-})
+  const mailOptions = {
+    from: process.env.MAIL_FROM,
+    to: "toluadeyemo7@gmail.com",
+    subject: "TEST EMAIL",
+    html: `<h4>Here is your mail</h4>
+        <p>${text}</p>`,
+  };
+
+  await transport.sendMail(mailOptions, function (err, data) {
+    if (err) console.log(err);
+    else console.log(data);
+  });
+});
 
 app.listen(process.env.PORT || 4000, () => {
-  console.log(`server is listening on port 4000, ${process.env.MAIL_PASSWORD}`);
+  console.log(`server is listening on port 4000, ${process.env.API_KEY}`);
 });
